b3081380a091d602112d0bc6d9f98e9bde0f38a6
keanseng04 <keanseng04@gmail.com>
2015-07-08 14:35:51 +0800

    Edited config action mailer production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'www.project-mod.com', protocol: 'https' }
+  config.action_mailer.default_url_options = {host: 'project-mod-staging.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end

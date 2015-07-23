91663e59505e9c8ee240218492ed542d28806a2e
Amanda Mariko Sakai <marikosakaii@gmail.com>
2015-07-23 15:45:36 +0800

    Finished salon list page, salon list responsiveness WIP and updated success page

--- app/assets/javascripts/outlets.js
+++ app/assets/javascripts/outlets.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  
+
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -27,5 +27,4 @@ $(document).ready(function(){
   }
 
   getLocation();
-
 })

0accdef7813ac5f2db12121ae46296ca0c7b2d9d
eldrethlim <eldrethlim@gmail.com>
2015-08-10 20:44:12 +0800

    added merchant sessions to check if user is merchant

--- app/controllers/merchants/sessions_controller.rb
+++ app/controllers/merchants/sessions_controller.rb
@@ -6,18 +6,18 @@ class Merchants::SessionsController < Merchants::BaseController
 
   def create
     @user = login(params[:merchants_sessions][:email], params[:merchants_sessions][:password])
+
+    return redirect_to(root_path, error: "You have entered the wrong email or password. Please try again!") unless @user
+    return redirect_to(root_path, error: "Sorry, you're not authorized to log in.") unless @user.merchant?
+    
     if @user.active_merchant?
-      flash[:success] = "Logged in!"
+      flash[:success] = "You've logged in, welcome back!"
       redirect_to merchants_dashboard_path
     else
       @outlet = @user.outlet
       redirect_to merchants_outlet_step_path(@outlet, "salon_info")
     end
-  rescue
-    flash[:error] = "You have entered the wrong email or password. Please try again!"
-    redirect_to root_path
   end
-
 
   def destroy
   end

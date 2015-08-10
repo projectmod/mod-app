f0f55fad69af73a38ee760d46002c0543f72e1fe
eldrethlim <eldrethlim@gmail.com>
2015-08-10 16:40:54 +0800

    added phone number uniqueness validation for user model, slight refactor user sessions controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,29 +3,29 @@ class UserSessionsController < ApplicationController
 
 	def new
 		@user = User.new
-		if params[:outlet]
-			session[:prebk_outlet] = params[:outlet]
-		end
+
+		session[:prebk_outlet] = params[:outlet] if params[:outlet]
 	end
 
 	def create
 		if @user = login(params[:user_sessions][:email], params[:user_sessions][:password])
-			if session[:prebk_outlet]
-				outlet = Outlet.find(session[:prebk_outlet])
-				session[:prebk_outlet] = nil
-				redirect_to(outlet_path(outlet), notice: "Login Successful")
-			else
-				redirect_back_or_to(root_path, notice: "Login successful.")
-			end
+			flash[:notice] = "You've succesfully logged in!"
+			outlet = Outlet.find_by(id: session[:prebk_outlet])
+			session[:prebk_outlet] = nil
+
+			return redirect_to outlet_path(outlet) if outlet
+
+			redirect_back_or_to root_path
 		else
-			flash[:notice] = 'Login failed.'
+
+			flash[:error] = 'We failed to log you in. Please try again!'
 			render action: 'new'
 		end
 	end
 
 	def destroy
 		logout
+		flash[:notice] = "You've logged out, see you again!"
 		redirect_to root_path
-		flash[:notice] = "Logged out."
 	end
 end

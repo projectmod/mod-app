a88791a8fc07c123ccd7f6e6df22dd3f4fe6383e
eldrethlim <eldrethlim@gmail.com>
2015-08-12 16:31:57 +0800

    added account page to link to merchant dashboard is user is a merchant

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,4 +3,14 @@ module UserHelper
   def bookings(credits)
     credits / 2
   end
+
+  def render_account_link
+    if current_user.merchant?
+      html = link_to("ACCOUNT", merchants_dashboard_path, class: "alignment nav-header-highlight")
+    else
+      html = link_to("ACCOUNT", dashboard_account_path, class: "#{is_active?('dashboard/account#index')} alignment nav-header-highlight")
+    end
+
+    html.html_safe
+  end
 end

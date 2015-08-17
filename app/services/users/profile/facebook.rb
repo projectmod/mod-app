76c327d22caa41c8f59df3e587398cf85c75501e
eldrethlim <eldrethlim@gmail.com>
2015-08-17 09:55:26 +0800

    slight refactoring

--- app/services/users/profile/facebook.rb
+++ app/services/users/profile/facebook.rb
@@ -1,21 +1,16 @@
-module Users
-  module Profile
-    class Facebook
-      attr_accessor :access_token
+class Users::Profile::Facebook
+  attr_accessor :access_token
 
-      def initialize(access_token)
-        @access_token = access_token
-      end
+  def initialize(access_token)
+    @access_token = access_token
+  end
 
-      def retrieve
-        graph = Koala::Facebook::API.new(access_token)
+  def retrieve
+    graph = Koala::Facebook::API.new(access_token)
 
-        profile = graph.get_object("me", {}, api_version: "v2.3")
-        image = graph.get_picture("me", {type: "large"})
+    profile = graph.get_object("me", {}, api_version: "v2.3")
+    image = graph.get_picture("me", {type: "large"})
 
-        OpenStruct.new(name: profile["name"], email: profile["email"], uid: profile["id"], avatar: image)
-
-      end
-    end
+    OpenStruct.new(name: profile["name"], email: profile["email"], uid: profile["id"], avatar: image)
   end
 end

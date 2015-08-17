739577192aa5c0b15ce28eebc551d8a323b2d0e4
eldrethlim <eldrethlim@gmail.com>
2015-08-17 16:49:02 +0800

    updated seeds file, fixed nested forms to delete and save properly for multiple fields, various styling fixes, form placeholder now has opacity, added resend code function, user registration now removed 0 if its the first digit

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $('form').on('click', '.add_fields', function(event) {
-    var new_field = $(this).data('fields');
-    $('.working-day-fields').append(new_field);
+    var time = new Date().getTime();
+    var regexp = new RegExp($(this).data('id'), 'g');
+    var new_field = $(this).data('fields').replace(regexp, time);
+    $('.working-day-fields').append(new_field)
     event.preventDefault();
   });
 })
--- app/helpers/nested_forms_helper.rb
+++ app/helpers/nested_forms_helper.rb
@@ -2,9 +2,14 @@ module NestedFormsHelper
 
   def add_nested_form_fields(f, association)
     new_object = f.object.send(association).klass.new
+    id = new_object.object_id
 
-    f.fields_for(association, new_object) do |builder|
+    new_field = f.fields_for(association, new_object, child_index: id) do |builder|
       render( "partials/form/" + association.to_s, builder: builder)
     end
+
+    link = link_to "<i class='fa fa-plus fa-2x'></i>".html_safe, '#', class: 'add_fields', data: { id: id, fields: new_field.gsub("\n", "")}
+
+    link.html_safe
   end
 end

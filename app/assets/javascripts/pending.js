a40bf1a3633bdb5943cb3a60194964f8d1ac4cad
eldrethlim <eldrethlim@gmail.com>
2015-08-17 17:44:23 +0800

    Confirmation waiting now set to 3 minutes instead 1

--- app/assets/javascripts/pending.js
+++ app/assets/javascripts/pending.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   if ($("#pending-booking").length > 0) {
     window.setTimeout(function() {
       window.location.href = 'result';
-    }, 60000);
+    }, 180000);
   }
 });

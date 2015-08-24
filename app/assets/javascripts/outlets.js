92d0fd4b7310bfef64a9de81a55fb026f3c65dbd
eldrethlim <eldrethlim@gmail.com>
2015-08-25 00:00:52 +0800

    slight navigration refactor

--- app/assets/javascripts/outlets.js
+++ app/assets/javascripts/outlets.js
@@ -1,27 +1,26 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+var success = function(pos){
+  var crd = pos.coords;
+  $('#longitude').val(crd.longitude);
+  $('#latitude').val(crd.latitude);
+};
+
+var error = function(err){
+};
+
+var getLocation = function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+};
+
 $(document).ready(function(){
-
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  var getLocation = function() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(success, error, options);
-      } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-  };
-
-  var success = function(pos){
-    var crd = pos.coords;
-    $('#longitude').val(crd.longitude);
-    $('#latitude').val(crd.latitude);
-  };
-
-  var error = function(err){
-  };
-
   getLocation();
 });

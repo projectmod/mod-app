987efe70f22017ea55275ce84acf2d4894d5d7cb
marikosakai <marikosakaii@gmail.com>
2015-08-25 10:34:36 +0800

    Removed comments

--- spec/controllers/merchants/outlets_controller_spec.rb
+++ spec/controllers/merchants/outlets_controller_spec.rb
@@ -1,30 +1,5 @@
-# require 'rails_helper'
-#
-# RSpec.describe Merchants::OutletsController do
-#   describe 'PUT /merchants/outlets/:outlet_id/steps/outlet_info' do
-#     let(:password) { Faker::Internet.password }
-#     let(:params) {
-#       {
-#         user: {
-#           name: Faker::Company.name,
-#           email: Faker::Internet.email,
-#           password: password,
-#           password_confirmation: password,
-#           phone_number: "127167787",
-#           business_registration: "Faker1234"
-#         }
-#       }
-#     }
-#     let!(:outlets) { create(:outlet, steps: :outlet_info) }
-#
-#     it 'saves outlet info to the database' do
-#       binding.pry
-#       put :update, params
-#       outlet = Outlet.first
-#       expect(outlet.name).to eq(params[:outlet][:name])
-#       expect(outlet.area).to eq(params[:outlet][:area])
-#       expect(outlet.address).to eq(params[:outlet][:address])
-#       # expect(outlet.working_hours).to eq(params[:outlet][:working_hours])
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Merchants::OutletsController do
+
+end
